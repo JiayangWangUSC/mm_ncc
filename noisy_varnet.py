86bdd95c178894df01d51541e8e5758c533479a0
JiayangWang <jiayangwQ@usc.edu>
2022-11-02 15:09:37 -0700

    varnet deep

--- noisy_varnet.py
+++ noisy_varnet.py
@@ -43,8 +43,8 @@ def toIm(kspace):
 
 # %% varnet loader
 from varnet import *
-cascades = 6
-chans = 20
+cascades = 8
+chans = 16
 recon_model = VarNet(
     num_cascades = cascades,
     sens_chans = 16,
