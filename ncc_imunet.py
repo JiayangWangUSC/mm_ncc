03634e5ab8fec188a9dd11e283fdfb9ce54b57f3
JiayangWang <jiayangwQ@usc.edu>
2023-01-20 15:20:17 -0800

    unet ncc acc6

--- ncc_imunet.py
+++ ncc_imunet.py
@@ -50,7 +50,7 @@ recon_model = Unet(
   num_pool_layers = 4,
   drop_prob = 0.0
 )
-recon_model = torch.load("/project/jhaldar_118/jiayangw/refnoise/model/imnet_ncc_acc6")
+recon_model = torch.load("/project/jhaldar_118/jiayangw/refnoise/model/imunet_ncc_acc6")
 #print(sum(p.numel() for p in recon_model.parameters() if p.requires_grad))
 
 # %% training settings
